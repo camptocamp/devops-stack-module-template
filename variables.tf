f2064e851ba7e8d627acb31e2912ad0a3e00a615
Gonçalo Heleno <33546359+lentidas@users.noreply.github.com>
2023-04-24 10:54:24 +0200

    fix(terraform): add missing default variables (#17)
    * fix(terraform): add missing default variables
    
    * docs(terraform-docs): generate docs and write to README.adoc
    
    ---------
    
    Co-authored-by: lentidas <lentidas@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -8,15 +8,34 @@ variable "cluster_name" {
   default     = "cluster"
 }
 
+variable "base_domain" {
+  description = "Base domain of the cluster. Value used for the ingress' URL of the application."
+  type        = string
+  default     = null
+}
+
+variable "cluster_issuer" {
+  description = "SSL certificate issuer to use. Usually you would configure this value as `letsencrypt-staging` or `letsencrypt-prod` on your root `*.tf` files."
+  type        = string
+  default     = "ca-issuer"
+}
+
 variable "argocd_namespace" {
   description = "Namespace used by Argo CD where the Application and AppProject resources should be created."
   type        = string
+  default     = "argocd"
 }
 
 variable "target_revision" {
   description = "Override of target revision of the application chart."
   type        = string
   default     = "v1.0.0" # x-release-please-version
+}
+
+variable "namespace" {
+  description = "Namespace where the applications's Kubernetes resources should be created. Namespace will be created in case it doesn't exist."
+  type        = string
+  default     = "<NAMESPACE>"
 }
 
 variable "helm_values" {
